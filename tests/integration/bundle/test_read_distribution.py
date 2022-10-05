d2a9477ddd79bce5e7caa05da20af923480e8d60
WRFitch <will.fitch@canonical.com>
2022-10-05 12:37:16 +0100

    added preliminary test_read_distribution test

---
+++ tests/integration/bundle/test_read_distribution.py
@@ -0,0 +1,84 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import asyncio
+import logging
+
+import pytest
+from mailmanclient import Client
+from pytest_operator.plugin import OpsTest
+
+from constants import PG, PGB
+from tests.integration.helpers.helpers import (
+    deploy_and_relate_application_with_pgbouncer,
+    deploy_postgres_bundle,
+    get_app_relation_databag,
+    get_backend_relation,
+    get_backend_user_pass,
+    get_cfg,
+    get_legacy_relation_username,
+    run_sql,
+    scale_application,
+    wait_for_relation_joined_between,
+)
+from tests.integration.helpers.postgresql_helpers import (
+    check_database_users_existence,
+    check_databases_creation,
+)
+
+logger = logging.getLogger(__name__)
+
+
+PSQL = "psql"
+
+
+@pytest.mark.dev
+@pytest.mark.bundle
+@pytest.mark.abort_on_fail
+async def test_read_distribution(ops_test: OpsTest):
+    """Check that read instance changed during reconnection to proxy.
+
+    Each new read connection should connect to a new readonly node.
+    """
+    async with ops_test.fast_forward():
+        await asyncio.gather(
+            await deploy_postgres_bundle(ops_test),
+            await ops_test.model.deploy(
+                "postgresql-charmers-postgresql-client",
+                application_name=PSQL,
+            ),
+        )
+        await scale_application(ops_test, PG, 3)
+
+        psql_relation = await ops_test.model.relate(f"{PSQL}:db", f"{PGB}:db-admin")
+        wait_for_relation_joined_between(ops_test, PGB, PSQL)
+        await ops_test.model.wait_for_idle(
+            apps=[PSQL, PG, PGB],
+            status="active",
+            timeout=600,
+        )
+
+
+    unit_name = f"{PSQL}/0"
+    psql_databag = await get_app_relation_databag(ops_test, unit_name, psql_relation.id)
+    pgpass = psql_databag.get("password")
+    user = psql_databag.get("user")
+    host = psql_databag.get("host")
+    port = psql_databag.get("port")
+    dbname = psql_databag.get("database")
+    assert None not in [pgpass, user, host, port, dbname], "databag incorrectly populated"
+
+    user_command = "SELECT reset_val FROM pg_settings WHERE name='listen_addresses';"
+    # get first IP
+    rtn, first_ip, err = await run_sql(
+        ops_test, unit_name, user_command, pgpass, user, host, port, dbname
+    )
+    assert rtn == 0, f"failed to run admin command {user_command}, {err}"
+
+    # get second IP
+    rtn, second_ip, err = await run_sql(
+        ops_test, unit_name, user_command, pgpass, user, host, port, dbname
+    )
+    assert rtn == 0, f"failed to run admin command {user_command}, {err}"
+
+    assert first_ip != second_ip
