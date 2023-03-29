6c9b37bfe548f9bfaf7503ad6020d1df3ecbce80
Dragomir Penev <dragomir.penev@canonical.com>
2023-03-30 00:55:22 +0300

    Don't wait for PGB when deploying the bundle

--- tests/integration/helpers/helpers.py
+++ tests/integration/helpers/helpers.py
@@ -2,7 +2,6 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import asyncio
 import json
 from multiprocessing import ProcessError
 from typing import Dict
@@ -229,19 +228,14 @@ def relation_exited(ops_test: OpsTest, endpoint_one: str, endpoint_two: str) -> 
     return False
 
 
-async def deploy_postgres_bundle(
-    ops_test: OpsTest, scale_pgbouncer: int = 1, scale_postgres: int = 2, timeout=(60 * 10)
-):
+async def deploy_postgres_bundle(ops_test: OpsTest, scale_postgres: int = 2, timeout=(60 * 10)):
     """Deploy postgresql bundle."""
     async with ops_test.fast_forward():
         await ops_test.model.deploy("./releases/latest/postgresql-bundle.yaml")
         wait_for_relation_joined_between(ops_test, PG, TLS_APP_NAME)
         wait_for_relation_joined_between(ops_test, PG, PGB)
-        await ops_test.model.wait_for_idle(apps=[PG, PGB, TLS_APP_NAME], timeout=timeout)
-        await asyncio.gather(
-            scale_application(ops_test, PGB, scale_pgbouncer),
-            scale_application(ops_test, PG, scale_postgres),
-        )
+        await ops_test.model.wait_for_idle(apps=[PG, TLS_APP_NAME], timeout=timeout)
+        await scale_application(ops_test, PG, scale_postgres)
         await ops_test.model.wait_for_idle(
             apps=[PG, TLS_APP_NAME], status="active", timeout=timeout
         )
