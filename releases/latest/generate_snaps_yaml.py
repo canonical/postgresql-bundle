f5ff7b0a36e5f6ea32ba727bd661c2e9833b8da9
Lucas Gameiro Borges <lucas.borges@canonical.com>
2024-09-27 15:25:39 +0000

    try new format

--- releases/latest/generate_snaps_yaml.py
+++ releases/latest/generate_snaps_yaml.py
@@ -1,14 +1,9 @@
 import yaml
 import argparse
 
-def convert_to_yaml(output_table_file, canonical_livepatch_file):
-    # Read the output_table.txt
-    with open(output_table_file, "r") as table_file:
-        table_lines = table_file.readlines()
-
-    # Read the canonical_livepatch.txt
-    with open(canonical_livepatch_file, "r") as livepatch_file:
-        livepatch_line = livepatch_file.read().strip()
+def convert_to_yaml(output_table, canonical_livepatch):
+    table_lines = output_table.splitlines()
+    livepatch_line = canonical_livepatch.strip()
 
     # Extract livepatch info
     livepatch_channel, livepatch_revision = livepatch_line.split(":")
@@ -40,8 +35,8 @@ def convert_to_yaml(output_table_file, canonical_livepatch_file):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert files to YAML.')
-    parser.add_argument('output_table', type=str, help='Path to output_table.txt')
-    parser.add_argument('canonical_livepatch', type=str, help='Path to canonical_livepatch.txt')
+    parser.add_argument('output_table', type=str, help='output_table')
+    parser.add_argument('canonical_livepatch', type=str, help='canonical_livepatch')
     args = parser.parse_args()
 
     convert_to_yaml(args.output_table, args.canonical_livepatch)
