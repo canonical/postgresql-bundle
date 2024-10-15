fbb2b291911a360c483b47cb0fc0712607b338ad
Paulo Machado <paulo.machado@canonical.com>
2024-10-15 17:41:17 -0300

    DPE-5627 Arch support and CC006 compliance (#217)
    * tf solution to bundle repo
    
    * charm revision override and any certificate charm support
    
    * fix bad c-c c-v

---
+++ terraform/revisions.tf
@@ -0,0 +1,22 @@
+locals {
+  postgresql_revisions = {
+    amd64 = 468,
+    arm64 = 467
+  }
+  s3_integrator_revisions = {
+    amd64 = 31,
+    arm64 = 32
+  }
+  data_integrator_revisions = {
+    amd64 = 41,
+    arm64 = 40
+  }
+  pgbouncer_revisions = {
+    amd64 = 396,
+    arm64 = 397
+  }
+  tls_revisions = {
+    amd64 = 155,
+    arm64 = 201
+  }
+}
